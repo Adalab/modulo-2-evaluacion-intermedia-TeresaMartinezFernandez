7303826eb9b7d6d948828f8a8ab911c6d0219787
TeresaMartinezFernandez <teresamf64@gmail.com>
2021-01-28 11:01:34 +0100

    add funcionality page

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,39 @@
 'use strict'
+
+        //funcion que genera numero aleatorio
+
+    function getRandomNumber(max) {
+        return Math.ceil(Math.random() * max);
+
+        }
+        const randomNumber = getRandomNumber(100);
+        console.log (randomNumber);
+
+        //me traigo los elementos del html
+
+        const inputBottonElement = document.querySelector('.js-inputbtn');
+        const inputValueElement = document.querySelector('.js-inputvalue');
+        const boxPistaElement = document.querySelector('.js-pista');
+
+
+        function handleUpdateBtn () {
+
+            const inputValue = inputValueElement.value;
+
+            if (inputValue > randomNumber) {
+                boxPistaElement.innerHTML = "Demasiado alto";
+                console.log ()
+            }
+            else if (inputValue < randomNumber) {
+                boxPistaElement.innerHTML = "Demasiado bajo";
+            }
+            else if (inputValue === randomNumber) {
+                boxPistaElement.innerHTML = "Has ganado campeona";
+            }
+            else if (inputValue >100 || inputValue <1) {
+                    boxPistaElement.innerHTML = "El numero debe estar entre 1 y 100";
+
+        }
+    }
+
+        inputBottonElement.addEventListener ('click', handleUpdateBtn);
