84fadb5bb6ba7deb449b23b97cc67f0b7ce13986
TeresaMartinezFernandez <teresamf64@gmail.com>
2021-02-04 16:08:50 +0100

    add issues

--- JS/main.js
+++ JS/main.js
@@ -1,38 +1,38 @@
-'use strict'
+"use strict";
 
-        
+const inputBottonElement = document.querySelector(".js-inputbtn");
+const inputValueElement = document.querySelector(".js-inputvalue");
+const boxPistaElement = document.querySelector(".js-pista");
+const counterElement = document.querySelector(".js-counter");
 
-    function getRandomNumber(max) {
-        return Math.ceil(Math.random() * max);
-        }
-        const randomNumber = getRandomNumber(100);
-        console.log (randomNumber);
+function numberGame() {
+  handleUpdateBtn();
+  incrementCounter();
+}
+const incrementCounter = () => {
+  let inputCounter = parseInt(counterElement.innerHTML);
+  counterElement.innerHTML = inputCounter + 1;
+  console.log(inputCounter);
+};
 
-        
+function handleUpdateBtn() {
+  const inputValue = parseInt(inputValueElement.value);
 
-        const inputBottonElement = document.querySelector('.js-inputbtn');
-        const inputValueElement = document.querySelector('.js-inputvalue');
-        const boxPistaElement = document.querySelector('.js-pista');
+  if (inputValue > 100 || inputValue < 1) {
+    boxPistaElement.innerHTML = "El numero debe estar entre 1 y 100";
+  } else if (inputValue > randomNumber) {
+    boxPistaElement.innerHTML = "Demasiado alto";
+  } else if (inputValue < randomNumber) {
+    boxPistaElement.innerHTML = "Demasiado bajo";
+  } else {
+    boxPistaElement.innerHTML = "Has ganado campeona";
+  }
+}
 
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+const randomNumber = getRandomNumber(100);
+console.log(randomNumber);
 
-        function handleUpdateBtn () {
-
-            const inputValue = inputValueElement.value;
-
-            if (inputValue > randomNumber) {
-                boxPistaElement.innerHTML = "Demasiado alto";
-                
-            }
-            else if (inputValue < randomNumber) {
-                boxPistaElement.innerHTML = "Demasiado bajo";
-            }
-            else if (inputValue === randomNumber) {
-                boxPistaElement.innerHTML = "Has ganado campeona";
-            }
-            else if (inputValue >100 || inputValue <1) {
-                    boxPistaElement.innerHTML = "El numero debe estar entre 1 y 100";
-
-        }
-    }
-
-        inputBottonElement.addEventListener ('click', handleUpdateBtn);
+inputBottonElement.addEventListener("click", numberGame);
